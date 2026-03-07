f832911ae7a8cc501ab9abc1481f0acf7e7868cb
Sean Boaden <sean.b0153@gmail.com>
2026-03-07 18:26:18 +0800

    enhancement: added banner to tv series/season views

--- apps/web/src/views/TvSeriesDetailView.vue
+++ apps/web/src/views/TvSeriesDetailView.vue
@@ -11,6 +11,11 @@ import Button from '@/components/ui/button/Button.vue'
 import { useTvSeriesStore } from '@/stores/tvSeries'
 import { TvSeason } from '@hls-app/sdk'
 import { RouterView, useRoute, useRouter } from 'vue-router'
+import Card from '@/components/ui/card/Card.vue'
+import { getImage } from '@/lib/utils'
+import moment from 'moment'
+import { truncate } from 'lodash'
+import { CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 
 const router = useRouter()
 const route = useRoute()
@@ -43,6 +48,30 @@ function getSeasonHref(tvSeason: TvSeason) {
       </Empty>
     </template>
     <template v-else>
+      <Card
+        class="col-span-full flex flex-col flex-1 h-96 w-full max-w-full relative rounded-md overflow-hidden"
+      >
+        <div
+          v-if="selectedTvSeries.banner"
+          class="absolute bg-cover bg-center w-full h-full z-0 rounded-lg"
+          :style="{ backgroundImage: `url('${getImage(selectedTvSeries.banner)}')` }"
+        >
+          <div class="overlay rounded-lg"></div>
+        </div>
+        <CardHeader class="z-10">
+          <CardTitle class="text-4xl font-bold text-white text-shadow-black text-shadow-sm">
+            {{ selectedTvSeries.name }}
+          </CardTitle>
+          <CardDescription class="max-w-1/3 text-white text-shadow-black text-shadow-sm">
+            {{ moment(selectedTvSeries.releaseDate).get('year') }}
+          </CardDescription>
+        </CardHeader>
+        <CardContent class="z-10">
+          <p class="description max-w-2/3 text-white text-shadow-sm text-shadow-black opacity-85">
+            {{ truncate(selectedTvSeries.description, { length: 500 }) }}
+          </p>
+        </CardContent>
+      </Card>
       <template v-if="selectedTvSeries.tvSeasons?.length">
         <MediaCard
           v-for="season of selectedTvSeries.tvSeasons"
--- apps/web/src/views/TvSeriesSeasonDetailView.vue
+++ apps/web/src/views/TvSeriesSeasonDetailView.vue
@@ -12,6 +12,10 @@ import { useTvSeriesStore } from '@/stores/tvSeries'
 import { Media } from '@hls-app/sdk'
 import { computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { getImage } from '@/lib/utils'
+import moment from 'moment'
+import { truncate } from 'lodash'
 
 const route = useRoute()
 const router = useRouter()
@@ -22,12 +26,14 @@ function getEpisodeHref(episode: Media) {
   return router.resolve({ name: 'WatchMedia', params: { mediaId: episode.id } }).href
 }
 
-const episodes = computed(() => {
+const season = computed(() => {
   const { season } = route.params
-  return (
-    selectedTvSeries.value?.tvSeasons?.find((tvSeason) => String(tvSeason.season) === season)
-      ?.mediaItems ?? []
-  )
+
+  return selectedTvSeries.value?.tvSeasons?.find((tvSeason) => String(tvSeason.season) === season)
+})
+
+const episodes = computed(() => {
+  return season.value?.mediaItems ?? []
 })
 </script>
 
@@ -35,7 +41,7 @@ const episodes = computed(() => {
   <div
     class="w-full h-auto max-h-[calc(100vh-96px)] rounded-md grid gap-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 overflow-y-auto"
   >
-    <template v-if="!selectedTvSeries">
+    <template v-if="!season">
       <Empty class="col-span-full">
         <EmptyHeader>
           <EmptyMedia>
@@ -50,6 +56,35 @@ const episodes = computed(() => {
       </Empty>
     </template>
     <template v-else>
+      <Card
+        class="col-span-full flex flex-col flex-1 h-96 w-full max-w-full relative rounded-md overflow-hidden"
+      >
+        <div
+          v-if="season.banner"
+          class="absolute bg-cover bg-center w-full h-full z-0 rounded-lg"
+          :style="{ backgroundImage: `url('${getImage(season.banner)}')` }"
+        >
+          <div class="overlay rounded-lg"></div>
+        </div>
+        <CardHeader class="z-10">
+          <CardTitle class="text-white text-shadow-black text-shadow-sm">
+            <span class="text-4xl font-bold mr-3">
+              {{ selectedTvSeries?.name }}
+            </span>
+            <span class="text-3xl opacity-80">
+              {{ season.name }}
+            </span>
+          </CardTitle>
+          <CardDescription class="max-w-2/3 text-white text-shadow-black text-shadow-sm">
+            {{ moment(season.releaseDate).get('year') }}
+          </CardDescription>
+        </CardHeader>
+        <CardContent class="z-10">
+          <p class="description max-w-2/3 text-white text-shadow-sm text-shadow-black opacity-85">
+            {{ truncate(season.description, { length: 500 }) }}
+          </p>
+        </CardContent>
+      </Card>
       <template v-if="episodes.length">
         <MediaCard
           v-for="episode of episodes"
